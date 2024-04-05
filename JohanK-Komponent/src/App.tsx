240a0845546556a3c58eabfabdf5f81769236bc5
JoohanK <94johkri1@skola.boras.se>
2024-04-05 15:55:00 +0200

    grunden, funktionalitet ok

--- JohanK-Komponent/src/App.tsx
+++ JohanK-Komponent/src/App.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import HamburgerMenu from "./components/HamburgerMenu/HamburgerMenu";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const menuItems = [
+    { label: "Home" },
+    { label: "About" },
+    { label: "Contact" },
+    { label: "Blog" },
+    { label: "Careers" },
+  ];
+
   return (
     <>
-      <HamburgerMenu />
+      <HamburgerMenu
+        isOpen={isOpen}
+        toggleMenu={toggleMenu}
+        menuItems={menuItems}
+        color="red"
+      />
     </>
   );
 }
--- JohanK-Komponent/src/components/HamburgerMenu/HamburgerMenu.tsx
+++ JohanK-Komponent/src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,12 +1,54 @@
 import React from "react";
 import "./HamburgerMenu.scss";
 
-function HamburgerMenu() {
+interface MenuItem {
+  label: string;
+}
+
+interface HamburgerMenuProps {
+  isOpen: boolean;
+  toggleMenu: () => void;
+  menuItems: MenuItem[];
+  color?: string;
+}
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
+  isOpen,
+  toggleMenu,
+  menuItems,
+  color = "black",
+}) => {
+  const getLineStyle = () => ({
+    backgroundColor: color,
+  });
+
   return (
-    <div>
-      <h1>Hamburger</h1>
+    <div className="HamburgerMenu">
+      <div onClick={toggleMenu} className={`hamburger ${isOpen ? "open" : ""}`}>
+        {!isOpen ? (
+          <>
+            <div className="line" style={getLineStyle()} />
+            <div className="line" style={getLineStyle()} />
+            <div className="line" style={getLineStyle()} />
+          </>
+        ) : (
+          <div className="x-icon" style={{ color: color }}>
+            ✕
+          </div>
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="menu">
+          {menuItems.map((menuItem, index) => (
+            <div key={index} className="menu-item" style={{ color: color }}>
+              {menuItem.label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default HamburgerMenu;
